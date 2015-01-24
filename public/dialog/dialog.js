417e64ae778dda4acd83c87aac35f7a53659d9ba
Aadhi Vive <avive@infusion.com>
2015-01-23 22:36:12 -0500

    changed to dialog folder

--- public/dialog/dialog.js
+++ public/dialog/dialog.js
@@ -2,33 +2,32 @@ var dialog = function(type,title,options){
 	this.type  = type;
 	this.options = options;
 	this.title = title;
-	this.create = function(){
-		var element = $("<p>" + this.title + "</p>");
+	this.dialog = (function(){
+		var element = $("<div class='dialog'><p>" + this.title + "</p><div class='actions'></div></div>");
 
 		switch(this.type){
 			case "start":
 
 				break;
 			case "alert":
-				element.append("<p>" + options + "</p>");
+				element.find(".actions").append("<p>" + options + "</p>");
 				break;
 			case "challengeSelect":
 				options.forEach(function(el,i){
 					var btn = $("<button class='dialog-btn'>" + el + "</button>");
-					element.prepend(btn);
+					element.find(".actions").append(btn);
 				});
 				break;
 			default:
 		}
 
 		return element;
-	};
+	})();
 
 };
 
 dialog.prototype.show = function(){
-	var dialog = this.create();
-	dialog.show();
+	this.dialog.show();
 };
 
 dialog.prototype.selection = function(callback) {
