5660db32c526aabd3dd6461f1ac919075eadb25e
Aadhi Vive <avive@infusion.com>
2015-01-23 23:05:52 -0500

    basic dialog added

--- public/dialog/dialog.js
+++ public/dialog/dialog.js
@@ -1,16 +1,17 @@
-var dialog = function(type,title,options){
+var Dialog = function (type,title,options){
 	this.type  = type;
 	this.options = options;
 	this.title = title;
-	this.dialog = (function(){
+	this.dialog = function(){
 		var element = $("<div class='dialog'><p>" + this.title + "</p><div class='actions'></div></div>");
 
 		switch(this.type){
 			case "start":
-
+				element.find(".actions" ).append("<button class='dialog-btn'>Start</button>");
 				break;
 			case "alert":
-				element.find(".actions").append("<p>" + options + "</p>");
+				var toAppend = (options) ? options : "Test";
+				element.find(".actions").append("<button class='dialog-btn'>" + toAppend + "</button>");
 				break;
 			case "challengeSelect":
 				options.forEach(function(el,i){
@@ -21,19 +22,21 @@ var dialog = function(type,title,options){
 			default:
 		}
 
+		element.find(".dialog-btn" ).click(this.close);
 		return element;
-	})();
+	};
 
+}
+
+Dialog.prototype.show = function(){
+	$("#dialog-holder").empty();
+	$("#dialog-holder").append(this.dialog());
 };
 
-dialog.prototype.show = function(){
-	this.dialog.show();
-};
-
-dialog.prototype.selection = function(callback) {
+Dialog.prototype.selection = function(callback) {
 	callback();
 };
 
-dialog.prototype.close = function(){
-
+Dialog.prototype.close = function(){
+	$("#dialog-holder").empty();
 };
