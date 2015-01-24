b8fc10ec2dd1340e8e1a17d834fb0946ad63c63f
Marc Pelland <mpelland@infusion.com>
2015-01-24 00:22:48 -0500

    improve structure of data in firebase add checks for player and room existance

--- public/sandbox/firebase/firebase.js
+++ public/sandbox/firebase/firebase.js
@@ -1,34 +1,70 @@
 $(function() {
 
 	var fbDataRef = new Firebase('https://snl-room.firebaseio.com/');
+
+	var roomUrl = 'https://snl-room.firebaseio.com/game/rooms';
+	var playerUrl = 'https://snl-room.firebaseio.com/game/players';
+	var moveUrl = 'https://snl-room.firebaseio.com/game/moves';
   	// var authData = fbDataRef.getAuth();
 
   	var roomId = "testRoom1";
   	var playerId = "testPlayer1";
   	var avatar = 1;
 
-  	var players = [];
-
-  	function registerRoom(room) {
-
-  		roomId = room;
-
-	    fbDataRef.push({
-	      action: "createRoom", 
-	      room: roomId
-	    });
-
+  	// register a new room
+  	function registerRoom() {
+        roomId = prompt('room id?', 'testRoom1');
+        checkIfRoomExists(roomId);
   	}
 
-  	function registerPlayer(playerId, avatar) {
-  		
-  		fbDataRef.child(roomId).push({
-	      action: "registerPlayer", 
-	      playerId: playerId,
-	      avatar: avatar
-	    });
+  	// room exist callback
+  	function roomExistsCallback(roomId, exists) {
+        if (exists) {
+        	// check if i can join the room
+          	alert('sorry, ' + roomId + ' exists already!');
+        } else {
+        	// create the room
+        	var roomsRef = new Firebase(roomUrl);
+        	roomsRef.child(roomId).set({name: roomId, open: true });
+        	alert('room ' + roomId + ' created');
+        }
+    }
 
+    // check to see if a room exists
+    function checkIfRoomExists(roomId) {
+		var roomsRef = new Firebase(roomUrl);
+		roomsRef.child(roomId).once('value', function(snapshot) {
+			var exists = (snapshot.val() !== null);
+			roomExistsCallback(roomId, exists);
+		});
+	}
+
+  	function registerPlayer(roomId) {
+  		playerId = prompt('player id?', 'testPlayer1');
+        checkIfPlayerExistsInRoom(roomId, playerId);
   	}
+
+  	// room exist callback
+  	function playerExistsCallback(roomId, playerId, exists) {
+        if (exists) {
+        	// check if i can join the room
+          	alert('sorry, ' + playerId + ' exists already in room ' + roomId + '!');
+        } else {
+        	// create the room
+        	var playersRef = new Firebase(playerUrl);
+        	playersRef.child(roomId).child(playerId).set({name: playerId, active: true, avatar: 1 });
+        	alert('player ' + playerId + ' created');
+        }
+    }
+
+    // check to see if a room exists
+    function checkIfPlayerExistsInRoom(roomId, playerId) {
+		var playersRef = new Firebase(playerUrl);
+		playersRef.child(roomId).child(playerId).once('value', function(snapshot) {
+			var exists = (snapshot.val() !== null);
+			playerExistsCallback(roomId, playerId, exists);
+		});
+	}
 
   	function postRoll(playerId, rollAmount) {
   		fbDataRef.child(roomId).child(playerId).set({
@@ -61,9 +97,9 @@ $(function() {
 	fbDataRef.child(roomId).on("child_removed", function(snapshot) {
 		console.log("REMOVED", snapshot);
 	});
-
-  	// registerRoom("testRoom1");
-  	// registerPlayer("testPlayer1", 1);
+// registerRoom();
+  	registerRoom();
+  	registerPlayer("testRoom1");
   	// postRoll("testRoom1", "testPlayer1", 4);
   	// movePlayer("testRoom1", "testPlayer1", -6);
   	// movePlayer("testRoom1", "testPlayer1", 4);
