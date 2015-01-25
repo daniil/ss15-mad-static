c6651258597706413bc31e91a1d283e3f52dec1d
Marc Pelland <mpelland@infusion.com>
2015-01-25 09:25:27 -0500

    git started ignorng game js file, so calling it main start the process of enabling mini-game challenges

--- public/js/main.js
+++ public/js/main.js
@@ -5,24 +5,32 @@
     	roomId: "",
     	activePlayers: {},
 
-    	initMainMenu: function() {
+        gameIsRunning: false,
+        dialog: null,
 
-    		$("#game-board").hide();
+        init: function() {
+            this.showMainMenu();
+        },
+
+    	showMainMenu: function() {
 
     		$("#room-create").on("click", function(e) {
 	            e.preventDefault();
+
 	            var room = $("#roomId").val();
+
 	            if (room.trim().length < 4) {
-	                this.displayError("ROOM NAME MUST BE LONGER THAT 3 CHARACTERS");
+	                game.displayError("ROOM NAME MUST BE LONGER THAT 3 CHARACTERS");
 	                return;
 	            }
+
 	            fb.registerRoom(room);
-	            // alert("Try to create the room " + room);
 	        });
 
 
 	        $("#room-join").on("click", function(e) {
 	            e.preventDefault();
+
 	            var room = $("#roomId").val();
 
 	            // make sure the room is valid
@@ -57,38 +65,65 @@
 
     	},
 
+        initBoard: function() {
+            
+            if (this.gameIsRunning) return;
+
+            $("#main-menu").fadeOut();
+            $("#game-board").fadeIn();
+
+            this.displayDialog("waiting");
+
+            $("#waiting .cta").on("click", function(e) {
+                game.hideDialog("waiting");
+                game.startGame();
+            });
+
+        },
+
+        startGame: function() {
+            if (this.gameIsRunning) return;
+            this.gameIsRunning = true;
+
+            // close the game off to others
+            fb.closeGame();
+
+            board.init();
+            challenges.init();
+
+            fb.initTurns();
+
+        },
+
     	playerChanged: function(data) {
     		// var playerId = data.name;
     		board.movePlayer(data.name, data.position)
     	},
 
         playerAdded: function(data) {
+
             this.activePlayers.push(data);
+            
             for (var i in this.activePlayers) {
                 var player = this.activePlayers[i]; 
                 var playerInfoDiv = $("<li id='legend-" + player.name + "'>" + player.name + "<div class='avatar" + player.avatar + "'></div></li>");
                 $("#activePlayers").append(playerInfoDiv);
 
-                board.addPlayer(player.name, "avatar" + player.avatar, player.position);
-                // this.playerChanged(player);
             }
         },
 
     	showPlayersInRoom: function() {
     		
-			this.initGame();
-            
-            $("#activePlayers").empty();
+            $("#activePlayers,#waitingPlayers").empty();
 
     		for (var i in this.activePlayers) {
     			var player = this.activePlayers[i]; 
     			var playerInfoDiv = $("<li id='legend-" + player.name + "'>" + player.name + "<div class='avatar" + player.avatar + "'></div></li>");
-    			$("#activePlayers").append(playerInfoDiv);
 
-    			board.addPlayer(player.name, "avatar" + player.avatar, player.position);
-    			// this.playerChanged(player);
+    			$("#activePlayers").append(playerInfoDiv);
+                $("#waitingPlayers").append(playerInfoDiv.clone());
     		}
-    		
+    	   
     	},
 
     	updateTurn: function(data) {
@@ -97,9 +132,10 @@
 
     		var player = data.order[data.currentPlayerTurn];
     		if (player == game.playerId) {
-    			this.displayMessage('it is your turn ' + game.playerId);
+    			setTimeout(function() { game.displayDialog('roll-dice'); }, 500);
                 dice.enableRoll();
     		} else {
+                // TODO: it is someone else's turn, make sure that they can go
                 dice.disableRoll();
             }
 
@@ -126,32 +162,69 @@
             }
         },
 
-    	initGame: function() {
-    		
-    		board.init();
+        landedOnSnake: function() {
+            $("#challenge-opponent-snake ul.challengePlayers").empty();
 
-            challenges.init();
+            for (var i in this.activePlayers) {
+                var player = this.activePlayers[i]; 
+                if (player.name != game.playerId) {
+                    var playerInfoDiv = $("<li id='challenge-" + player.name + "' class='challenge' data-name='" + player.name + "'>" + player.name + "<div class='avatar" + player.avatar + "'></div></li>");
 
-    		$("#main-menu").hide();
-    		$("#game-board").show();
+                    $("#challenge-opponent-snake ul.challengePlayers").append(playerInfoDiv);
 
-    	},
+                    $("#challenge-opponent-snake ul.challengePlayers li").on("click", game.challengeMiniGame);
+                }
+            }
+
+            this.displayDialog("challenge-opponent-snake");
+        },
+
+        landedOnLadder: function() {
+            $("#challenge-opponent-ladder ul.challengePlayers").empty();
+
+            for (var i in this.activePlayers) {
+                var player = this.activePlayers[i]; 
+                if (player.name != game.playerId) {
+                    var playerInfoDiv = $("<li id='challenge-" + player.name + "' class='challenge' data-name='" + player.name + "'>" + player.name + "<div class='avatar" + player.avatar + "'></div></li>");
+                    $("#challenge-opponent-ladder ul.challengePlayers").append(playerInfoDiv);
+                    $("#challenge-opponent-ladder ul.challengePlayers li").on("click", game.challengeMiniGame);    
+                }
+            }
+
+            this.displayDialog("challenge-opponent-ladder");
+        },
+
+        challengeMiniGame: function(e) {
+            game.challengePlayer(game.playerId, $(e.currentTarget).data("name"));
+        },
 
         challengePlayer: function(player1Id, player2Id) {
             challenges.startChallenge(player1Id, player2Id);
         },
 
+        displayDialog: function(dialog) {
+            $("#"+dialog+",#dialogs").fadeIn("fast");
+        },
+
+        hideDialog: function(dialog) {
+            $("#"+dialog+",#dialogs").fadeOut("slow");
+        },
+
     	displayMessage: function(message) {
             if ($("#message").hasClass("error")) $("#message").removeClass("error");
             $("#message>p").html(message);
+
+            $("#message,#non-interactive-dialogs").fadeIn("fast");
+            setTimeout(function() { $("#message,#non-interactive-dialogs").fadeOut("slow"); }, 3000);
         },
 
         displayError: function(message) {
             if (!$("#message").hasClass("error")) $("#message").addClass("error");
             $("#message>p").html(message);
+
+            $("#message,#non-interactive-dialogs").fadeIn("fast");
+            setTimeout(function() { $("#message,#non-interactive-dialogs").fadeOut("slow"); }, 3000);
         }
-
-
 
     };
 
