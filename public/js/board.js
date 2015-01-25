f3b4bda5f6ad9ded8cf8098952babc83b382d39a
Marc Pelland <mpelland@infusion.com>
2015-01-25 13:02:12 -0500

    update test environment for dynamic snakes and ladders

--- public/js/board.js
+++ public/js/board.js
@@ -58,6 +58,41 @@
                 tile.addClass('ladder-tile');
             }
 
+
+
+            $(".snake-tile").on("click", function() {
+                
+                var number = $(this).find(".snake").data("value");
+                var tileNum = Number($(this).attr("id").replace("tile-", ""));
+
+                for (var i = 0; i < number; i++) {
+                    if (i == 0) {
+                        $("#tile-" + board.pad(tileNum - i, 2)).addClass("snake-head");
+                    } else if (i == number - 1) {
+                        $("#tile-" + board.pad(tileNum - i, 2)).addClass("snake-tail");
+                    } else {
+                        $("#tile-" + board.pad(tileNum - i, 2)).addClass("snake-body");
+                    }
+                }
+            });
+
+            $(".ladder-tile").on("click", function() {
+
+                var number = $(this).find(".ladder").data("value");
+                var tileNum = Number($(this).attr("id").replace("tile-", ""));
+
+                for (var i = 0; i < number; i++) {
+                    if (i == 0) {
+                        $("#tile-" + board.pad(tileNum + i, 2)).addClass("ladder-tail");
+                    } else if (i == number - 1) {
+                        $("#tile-" + board.pad(tileNum + i, 2)).addClass("ladder-head");
+                    } else {
+                        $("#tile-" + board.pad(tileNum + i, 2)).addClass("ladder-body");
+                    }
+                }
+
+            });
+
             for (var i in game.activePlayers) {
                 
                 var player = game.activePlayers[i];
@@ -143,7 +178,10 @@
 
         moveBird: function() {
             var pindex = $("#" + board.activePlayer).data("index");
-
+            
+            console.log("pindex", pindex);
+            console.log("board.targetIndex", board.targetIndex);
+            
             if (board.targetIndex - 1 == pindex) {
                 
                 if ($("#tile-" + board.pad(pindex + 1, 2)).hasClass("ladder-tile"))  {
