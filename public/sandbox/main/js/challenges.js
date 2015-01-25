5c5aedbaf373ae7eefe365ee5d84ad7bb61da8c0
Daniil Molodkov <mr.me@daniil.ca>
2015-01-24 20:58:19 -0500

    Continued on simon integration

--- public/sandbox/main/js/challenges.js
+++ public/sandbox/main/js/challenges.js
@@ -4,17 +4,36 @@
     challengeRef: new Firebase(fb.challengeUrl),
 
     startChallenge: function(player1Id, player2Id) {
-      var players = this.challengeRef.child(game.roomId).child(players);
+      var room = this.challengeRef.child(game.roomId),
+          playersObj = {};
 
-        players.child(player1Id).set({
-          role: 'lead',
-          score: 0
-        });
+      playersObj[player1Id] = {
+        role: 'lead',
+        score: 0
+      };
 
-        players.child(player2Id).set({
-          role: 'wait',
-          score: 0
-        });
+      playersObj[player2Id] = {
+        role: 'wait',
+        score: 0
+      };
+
+      room.set({
+        players: playersObj
+      });
+
+      room.child('players').child(game.playerId).on('value', function(s) {
+        console.log('load game');
+      });
+
+        // players.child(player1Id).set({
+        //   role: 'lead',
+        //   score: 0
+        // });
+
+        // players.child(player2Id).set({
+        //   role: 'wait',
+        //   score: 0
+        // });
     }
 
   };
